b46f13bf33a67d3cd2e62eb690e7b231aaad9418
Saket Kumar Sinha <133163978+sinha-19@users.noreply.github.com>
2025-06-08 00:02:29 +0530

    Create ZFunctionAlgorithm.java

---
+++ Day 4/ZFunctionAlgorithm.java
@@ -0,0 +1,23 @@
+public class ZFunctionAlgorithm {
+    /*
+     * Problem: Compute Z-array where Z[i] = length of the longest substring
+     * starting from i which is also a prefix of the string.
+     * Time: O(N)
+     */
+    public static int[] computeZ(String s) {
+        int n = s.length();
+        int[] z = new int[n];
+        int l = 0, r = 0;
+        for (int i = 1; i < n; i++) {
+            if (i <= r)
+                z[i] = Math.min(r - i + 1, z[i - l]);
+            while (i + z[i] < n && s.charAt(z[i]) == s.charAt(i + z[i]))
+                z[i]++;
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+        return z;
+    }
+}
